c5179e31b4898a7c9632386d2ab440add031071f
IngridLop31 <janethlemuz55@gmail.com>
2025-04-24 14:23:31 -0500

    Update system_info.sh

--- system_info.sh
+++ system_info.sh
@@ -5,32 +5,38 @@ echo "Hostname: $(hostname)"
 echo ""
 # Display the operating system and version
 echo "OS and Version: $(uname -a)"
+echo ""
 
 # Display the uptime
 echo "Uptime: $(uptime -p)"
+echo ""
 
 # Display the memory usage
 echo "Memory Usage:"
 free -h
+echo ""
 
 # Display the disk usage
 echo "Disk Usage:"
 df -h
+echo ""
 
 # Display the CPU information
 echo "CPU Info:"
 lscpu | grep 'Model name'
+echo ""
 
 # Display the network interfaces
 echo "Network Interfaces:"
 ip -brief addr
 echo ""
 
- # Display detailed hardware information
- echo "Hardware Information:"
- lshw -short
+# Display detailed hardware information
+echo "Hardware Information:"
+lshw -short
+echo ""
 
-  # Display the number of installed packages
- echo "Number of Installed Packages:"
- dpkg -l | wc -l
+# Display the number of installed packages
+echo "Number of Installed Packages:"
+dpkg -l | wc -l
  
